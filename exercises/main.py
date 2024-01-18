f42fb8efa792671b5d716628d0cb9a8012b08c44
mdabrowski-phd <micdab90@gmail.com>
2024-01-18 19:31:04 +0100

    DELETE and PUT methods implemented

--- exercises/main.py
+++ exercises/main.py
@@ -1,6 +1,6 @@
 import random
 
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Response
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
@@ -34,22 +34,24 @@ def get_item_by_id(items_list, id_):
 
     for item in items_list:
         if item["id"] == id_:
-            result = item
-            break
-    else:
-        result = None
+            return item  # by default: return None
 
-    return result
+
+def get_item_index_by_id(items_list, id_):
+
+    for i, item in enumerate(items_list):
+        if item["id"] == id_:
+            return i  # by default: return None
 
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"message": "Hello World"})
 
 
 @app.get("/tasks")
 def get_tasks():
-    return JSONResponse(status_code=status.HTTP_202_ACCEPTED, content={"result": tasks_data})
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"result": tasks_data})
 
 
 @app.get("/tasks/{task_id}")
@@ -60,12 +62,12 @@ def get_task_by_id(task_id: int):
         message = {"error": f"Task with id {task_id} does not exist"}
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=message)
 
-    return JSONResponse(status_code=status.HTTP_202_ACCEPTED, content={"result": target_task})
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"result": target_task})
 
 
 @app.get("/users")
 def get_users():
-    return JSONResponse(status_code=status.HTTP_202_ACCEPTED, content={"result": users_data})
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"result": users_data})
 
 
 @app.get("/users/{user_id}")
@@ -76,7 +78,7 @@ def get_user_by_id(user_id: int):
         message = {"error": f"User with id {user_id} does not exist"}
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=message)
 
-    return JSONResponse(status_code=status.HTTP_202_ACCEPTED, content={"result": target_user})
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"result": target_user})
 
 
 @app.post("/tasks", status_code=status.HTTP_201_CREATED)
@@ -99,3 +101,59 @@ def create_user(body: UserBody):
     users_data.append(new_user)
 
     return {"message": "New user added", "details": new_user}
+
+
+@app.delete("/tasks/{task_id}")
+def delete_task_by_id(task_id: int):
+    target_index = get_item_index_by_id(tasks_data, task_id)
+
+    if target_index is None:
+        message = {"error": f"Task with id {task_id} not found"}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=message)
+
+    tasks_data.pop(target_index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.delete("/users/{user_id}")
+def delete_user_by_id(user_id: int):
+    target_index = get_item_index_by_id(users_data, user_id)
+
+    if target_index is None:
+        message = {"error": f"User with id {user_id} not found"}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=message)
+
+    users_data.pop(target_index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/tasks/{task_id}")
+def update_task_by_id(task_id: int, body: TaskBody):
+    target_index = get_item_index_by_id(tasks_data, task_id)
+
+    if target_index is None:
+        message = {"error": f"Task with id {task_id} does not exist"}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=message)
+
+    updated_task = body.model_dump()
+    updated_task["id"] = task_id
+    tasks_data[target_index] = updated_task
+
+    message = {"message": f"Task with id {task_id} updated", "new_value": updated_task}
+    return JSONResponse(status_code=status.HTTP_200_OK, content=message)
+
+
+@app.put("/users/{user_id}")
+def update_user_by_id(user_id: int, body: UserBody):
+    target_index = get_item_index_by_id(users_data, user_id)
+
+    if target_index is None:
+        message = {"error": f"User with id {user_id} does not exist"}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=message)
+
+    updated_user = body.model_dump()
+    updated_user["id"] = user_id
+    users_data[target_index] = updated_user
+
+    message = {"message": f"User with id {user_id} updated", "new_value": updated_user}
+    return JSONResponse(status_code=status.HTTP_200_OK, content=message)
