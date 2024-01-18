aec2ee0d13fa441cfb45676adbfaa55768d019c8
mdabrowski-phd <micdab90@gmail.com>
2024-01-18 20:23:04 +0100

    new file structure works

---
+++ exercises/src/app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
+
+from app.routers import tasks, users
+
+app = FastAPI()
+
+app.include_router(tasks.router)
+app.include_router(users.router)
+
+
+@app.get("/")
+def root():
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"message": "Hello World"})
---
+++ exercises/src/app/utils.py
@@ -0,0 +1,12 @@
+def get_item_by_id(items_list, id_):
+
+    for item in items_list:
+        if item["id"] == id_:
+            return item  # by default: return None
+
+
+def get_item_index_by_id(items_list, id_):
+
+    for i, item in enumerate(items_list):
+        if item["id"] == id_:
+            return i  # by default: return None
